0d5ef1a347d431aa31c94d57a34cc367ff11f165
f-diallo <56490509+f-diallo@users.noreply.github.com>
2021-04-09 20:50:01 -0700

    Add Final 2 mechanics

--- docs/index.md
+++ docs/index.md
@@ -5,10 +5,10 @@ Here are my 5 adjusted mechanics:
     - *It is modified to move diagonal with the Q and E keys.*
 - [Space to jump](build_webgl_space2jump)
     - *It is modified to slightly shift to the right.*
-<!-- - [Space to shoot]()
-    - *It is modified to shoot in other directions with the WASD keys.*
-- [Load next scene]()
-    - ** -->
+- [Space to shoot](build_webgl_space2shoot)
+    - *It is modified to drop grenades.*
+- [Load next scene](build_webgl_load_next_scene)
+    - *It is modified to have an additional level 3 with a door to it on level 1.*
 - [Move camera](build_webgl_move_camera)
     - *It is modified to zoom in with the N key and out with the M key.*  
 
---
+++ move_camera_2d/Assets/CameraController.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public float panSpeed = 20f;
+    public float zoomSpeed = 2f;
+    public float minZoom = 2f;
+    public float maxZoom = 15f;
+    public float panBorderThickness = 30f;
+    public Vector2 panLimit;
+
+    private void Update()
+    {
+        Vector3 pos = transform.position;
+        Camera cam = Camera.current;
+
+        if (Input.GetKey(KeyCode.W) || Input.mousePosition.y >= Screen.height - panBorderThickness)
+        {
+            pos.y += panSpeed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.S) || Input.mousePosition.y <= panBorderThickness)
+        {
+            pos.y -= panSpeed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.D) || Input.mousePosition.x >= Screen.width - panBorderThickness)
+        {
+            pos.x += panSpeed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.A) || Input.mousePosition.x <= panBorderThickness)
+        {
+            pos.x -= panSpeed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            pos.x = 0;
+            pos.y = 0;
+        }
+
+        if (Input.GetKey(KeyCode.N))
+        {
+            //cam.orthographicSize += panSpeed * Time.deltaTime;
+            cam.orthographicSize = Mathf.Lerp(cam.orthographicSize, minZoom, zoomSpeed * Time.deltaTime);
+
+        }
+        
+        if (Input.GetKey(KeyCode.M))
+        {
+            //cam.orthographicSize -= panSpeed * Time.deltaTime;
+            cam.orthographicSize = Mathf.Lerp(cam.orthographicSize, maxZoom, zoomSpeed * Time.deltaTime);
+        }
+
+        pos.x = Mathf.Clamp(pos.x, -panLimit.x, panLimit.x);
+        pos.y = Mathf.Clamp(pos.y, -panLimit.y, panLimit.y);
+          
+        transform.position = pos;
+    }
+}
